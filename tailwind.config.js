ececeefb299c745f364f674b5549d119f55f5eeb
Bojan Petkovic <bojan.petkovic@asioso.com>
2022-08-01 16:59:34 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit', // Just-In-Time Compiler
+  purge: ['./views/**/*.hbs'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
